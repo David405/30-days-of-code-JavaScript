4df182bde9c05cd56a82cef1689a53ec735b22bd
david405 <davidasamonye@gmail.com>
2019-06-07 17:08:55 +0100

    day 3

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+function main() {
+    const N = parseInt(readLine(), 10);
+
+    if (N % 2 == 1) {
+        console.log("Weird");
+    } else if (N % 2 == 0 && N >= 6 && N <= 20) {
+        console.log("Weird");
+    } else if (N % 2 == 0 && N >= 2 && N <= 5 ) {
+        console.log("Not Weird");
+    } else if (N % 2 == 0 && N >= 20) {
+        console.log("Not Weird");
+    }
+}
--- datatypes.js
+++ datatypes.js
@@ -6,8 +6,7 @@ function main() {
     var i = 4
     var d = 4.0
     var s = "HackerRank "
-    // Declare second integer, double, and String variables.
-    // Read and save an integer, double, and String to your variables.
+  
     var int = 12;
     var double = 4.0;
     var string = "is the best place to learn and practice coding!";
@@ -15,11 +14,10 @@ function main() {
     int = parseInt(readLine());
     double = parseFloat(readLine());
     string = readLine();
-    // Print the sum of both integer variables on a new line.
+
     console.log(i + int);
-    // Print the sum of the double variables on a new line.
+
     console.log((d + double).toFixed(1));
-    // Concatenate and print the String variables on a new line
-    // The 's' variable above should be printed first.
+  
     console.log(s + string);
 }
