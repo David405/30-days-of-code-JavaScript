c92086ea4584322de9bb1ec235c650d5fc81e9f6
david405 <davidasamonye@gmail.com>
2019-06-18 13:18:56 +0100

    day 15

---
+++ linkedlist.js
@@ -0,0 +1,16 @@
+this.insert=function(head,data){
+    //complete this method
+  var node = new Node(data);
+
+  if (head == null) {
+      head = node
+  } else {
+      var temp = head;
+
+      while (temp.next != null)
+          temp = temp.next
+      temp.next = node;
+  }
+
+  return head;
+};
