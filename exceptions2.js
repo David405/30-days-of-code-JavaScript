280f920525d5f95fb13aedb015faa32cdb875cf6
david405 <davidasamonye@gmail.com>
2019-06-27 11:24:07 +0100

    day 17

---
+++ exceptions2.js
@@ -0,0 +1,10 @@
+var Calculator = function() {
+    Calculator.prototype.power = function(n,p) {
+        
+        if (n < 0 || p < 0)
+            throw ("n and p should be non-negative");
+        else
+            var x = Math.pow(n,p);
+        return x;
+    }
+}
